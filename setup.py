757506fc290dce304b741995fff99a60e6e53f6a
Nathan Bergey <nathan.bergey@gmail.com>
2013-10-06 12:07:24 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='thingihub',
+    version='0.1',
+    description='Convert cad stuff to thingiverse-looking github pages',
+    author='Nathan Bergey',
+    author_email='nathan.bergey@gmail.com',
+    url='https://github.com/natronics/thingihub',
+    scripts = [
+        'scripts/thingihub'
+    ]
+)
